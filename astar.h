fa6b7e38f0541d80fe552666262eddc155d6c285
Wwbbbbb <267057047@qq.com>
2024-03-15 17:33:11 +0800

    增加了H

--- astar.h
+++ astar.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <list.h>
-#include <structs.h>
+#include <math.h>
 
 Pos AstarPath[400]; 
 node openlist[];
@@ -9,37 +9,42 @@ node openlist[];
 int isValid(int row, int col);//越界判断
 int calculateHValue(int row, int col, Pos dest);//H值计算函数
 void tracePath(Node nodeDetails[][COL], Pos dest);//回溯生成path
-void aStarSearch(int grid[][COL], Pair src, Pair dest){
+// void aStarSearch(int grid[][COL], Pair src, Pair dest){
 
-  OPEN = priority queue containing START
+//   OPEN = priority queue containing START
 
-CLOSED = empty set
+// CLOSED = empty set
 
-while lowest rank in OPEN is not the GOAL:
+// while lowest rank in OPEN is not the GOAL:
 
-  current = remove lowest rank item from OPEN
+//   current = remove lowest rank item from OPEN
 
-  add current to CLOSED
+//   add current to CLOSED
 
-  for neighbors of current:
+//   for neighbors of current:
 
-    cost = g(current) + movementcost(current, neighbor)
+//     cost = g(current) + movementcost(current, neighbor)
 
-    if neighbor in OPEN and cost less than g(neighbor):
+//     if neighbor in OPEN and cost less than g(neighbor):
 
-      remove neighbor from OPEN, because new path is better
+//       remove neighbor from OPEN, because new path is better
 
-    if neighbor in CLOSED and cost less than g(neighbor): **
+//     if neighbor in CLOSED and cost less than g(neighbor): **
 
-      remove neighbor from CLOSED
+//       remove neighbor from CLOSED
 
-    if neighbor not in OPEN and neighbor not in CLOSED:
+//     if neighbor not in OPEN and neighbor not in CLOSED:
 
-      set g(neighbor) to cost
+//       set g(neighbor) to cost
 
-      add neighbor to OPEN
+//       add neighbor to OPEN
 
-      set priority queue rank to g(neighbor) + h(neighbor)
+//       set priority queue rank to g(neighbor) + h(neighbor)
 
-      set neighbor's parent to current
+//       set neighbor's parent to current
+// }
+
+void getFdistance(Grid *grid, Goods *goods){
+    int hdistance;
+    hdistance = abs(grid->x - goods->x) + abs(grid->y - goods->y);
 }
