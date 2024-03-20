4d98e7bbfec5fffed26a6ef7a8d0684046ff1ad2
cr26696 <2669648220@qq.com>
2024-03-20 08:25:40 +0800

    bug修复

--- astar.h
+++ astar.h
@@ -12,6 +12,6 @@
 int isValidGrid(Map *map, Point point);//返回1表示空地
 LinkList* aStarSearch( struct Map *map, Point Psrc, Point Pdest);//返回一个path
 
-LinkList* generatePath(Grid temp);//传入current的拷贝
+LinkList* generatePath(Grid *temp);//传入current的拷贝
 
 #endif
