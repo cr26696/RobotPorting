b4182a4474cbb31c13cbc3cc764fa1b1b27be15f
Wwbbbbb <267057047@qq.com>
2024-03-21 22:44:35 +0800

    增加了小根堆 还没写缺少二叉树操作

---
+++ heap.h
@@ -0,0 +1,14 @@
+#ifndef HEAP_H//预定义避免重复引入
+#define HEAP_H
+
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void MaxHeadAdjust(int A[], int k, int len);
+void MinHeadAdjust(int A[], int k, int len);
+void BuildMaxHeap(int A[], int len);
+void BuildMinHeap(int A[], int len);
+void MaxHeapSort(int A[], int len);
+void MinHeapSort(int A[], int len);
+
+#endif
